82c218f771449cddfc3aa3b1a730ea5282635a06
Sascha Klement <sascha.r.klement@gmail.com>
2026-04-09 14:06:53 +0200

    Add quantity update and removeitem UI in shopping bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,6 +11,7 @@ def bag_contents(request):
     bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
+
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
@@ -20,7 +21,7 @@ def bag_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
-
+            
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
@@ -31,8 +32,7 @@ def bag_contents(request):
                     'quantity': quantity,
                     'product': product,
                     'size': size,
-            })
-
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -42,7 +42,7 @@ def bag_contents(request):
         free_delivery_delta = 0
     
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
