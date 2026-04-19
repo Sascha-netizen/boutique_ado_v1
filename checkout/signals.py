3b77907cdf93bfee65613cd04cef8afae41af6c5
Sascha Klement <sascha.r.klement@gmail.com>
2026-04-19 15:05:05 +0200

    Implement payment functionality and create checkout_success.html template

--- checkout/signals.py
+++ checkout/signals.py
@@ -11,9 +11,7 @@ def update_on_save(sender, instance, created, **kwargs):
     instance.order.update_total()
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
-    # chris made a mistake here, this function should have 
-    # been called update_on_delete. He fixes this in an upcoming video.
+def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
