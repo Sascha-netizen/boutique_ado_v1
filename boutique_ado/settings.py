5b2234e4109988471f0ebe50a106713a88740a09
Sascha Klement <sascha.r.klement@gmail.com>
2026-04-23 15:57:25 +0200

    Add profiles app

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -49,11 +49,13 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
-    'checkout',	
+    'checkout',
+    'profiles',	
 
     # Other
     'crispy_forms',
     'django_countries'
+    
 ]
 
 MIDDLEWARE = [
