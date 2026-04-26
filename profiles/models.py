959b507c3243919465f36dc1fe78b98b72912899
Sascha Klement <sascha.r.klement@gmail.com>
2026-04-26 15:12:01 +0200

    Customise allauth templates and fix user profile signal

--- profiles/models.py
+++ profiles/models.py
@@ -28,9 +28,9 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-        Create or update the user profile
+    Create or update the user profile
     """
     if created:
         UserProfile.objects.create(user=instance)
-    #Existing users just save the profile
-    instance.userprofile.save()
+    else:
+        instance.userprofile.save()
