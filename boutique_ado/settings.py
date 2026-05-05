66ef4d656eb8dc16da2e6651bedbf44c35cb62b5
Sascha Klement <sascha.r.klement@gmail.com>
2026-05-05 17:06:59 +0200

    Prepare project for Heroku deployment.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bf_nx7m0sd(kbofoqv1g*ba))$ux)2(ju#mr-kr5#21@5iql_d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1',
+ALLOWED_HOSTS = ['boutique-ado-practice-project-5cc6d1fbf45b.herokuapp.com',
+                '127.0.0.1',
                  'localhost'
 ]
 
@@ -127,12 +128,18 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 #Database
 #https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
- }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
